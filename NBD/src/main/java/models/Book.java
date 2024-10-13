3eebd49f5f8e0d6b9693799e1b341fddf6c676e6
Natalia Nosowicz <247745@edu.p.lodz.pl>
2024-10-13 21:30:14 +0200

    nn

--- NBD/src/main/java/models/Book.java
+++ NBD/src/main/java/models/Book.java
@@ -1,4 +1,13 @@
 package models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Table
+@Entity
 public class Book {
+    @Id
+    private Long id;
+
+
 }
